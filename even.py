652e3e57a23b253224fd53480e59fe6b22bfb610
glakshkar <gouravlakshkar801@gmail.com>
2025-10-06 20:42:09 +0530

    new file added

---
+++ even.py
@@ -0,0 +1,18 @@
+# n = int(input("Enter a number: "))
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+# n = int(input("Enter a number: "))
+# if n%2!=0:
+#     print("odd")
+# else:
+#     print("even")
+
+
+
+
+
+
